c475a7d62ce8c6a5ccbc2a40983c5634b02988c8
LEOVELFERT <leovelbertv@gmail.com>
2024-12-18 11:34:55 +0530

    file has been added

--- src/streamapi/limit/FindFirstTwo.java
+++ src/streamapi/limit/FindFirstTwo.java
@@ -7,7 +7,11 @@ import java.util.List;
 public class FindFirstTwo {
     public static void main(String[] args) {
         List<String> names= Arrays.asList("Gopi","leo","sunday","monday");
-        List<String> sortedList = names.stream().limit(2).toList();
+        List<String> sortedList = names
+                .stream()
+                .limit(2)
+                .skip(1)
+                .toList();
         System.out.println(sortedList);
     }
 }
