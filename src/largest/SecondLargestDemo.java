eec866f8b4c29c6ff8b82056ec90916bba37fd8c
Leovelbert V <leovelbertv@gmail.com>
2026-07-07 09:44:05 +0530

    Factors of numbers.

--- src/largest/SecondLargestDemo.java
+++ src/largest/SecondLargestDemo.java
@@ -5,12 +5,12 @@ import java.util.Comparator;
 
 public class SecondLargestDemo {
     public static void main(String[] args) {
-        int[] inputArray={2,4,5,7,8,9};
-        int secondLargestElement=getSecondLargest(inputArray);
-        System.out.println("the second largest element is "+secondLargestElement);
+        int[] inputArray = {2, 4, 5, 7, 8, 9};
+        int secondLargestElement = getSecondLargest(inputArray);
+        System.out.println("the second largest element is " + secondLargestElement);
 
-        secondLargestElement=getSecondLargestByStram(inputArray);
-        System.out.println("the second largest by using stream "+secondLargestElement);
+        secondLargestElement = getSecondLargestByStram(inputArray);
+        System.out.println("the second largest by using stream " + secondLargestElement);
     }
 
     private static int getSecondLargestByStram(int[] inputArray) {
@@ -22,14 +22,14 @@ public class SecondLargestDemo {
     }
 
     private static int getSecondLargest(int[] inputArray) {
-        int largest=Integer.MIN_VALUE;
-        int secondLargest=Integer.MIN_VALUE;
-        for(int i=0;i<inputArray.length;i++){
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < inputArray.length; i++) {
             if (inputArray[i] > largest) {
-                secondLargest=largest;
-largest=inputArray[i];
-            } else if (inputArray[i]<largest&&inputArray[i]>secondLargest) {
-                secondLargest=inputArray[i];
+                secondLargest = largest;
+                largest = inputArray[i];
+            } else if (inputArray[i] < largest && inputArray[i] > secondLargest) {
+                secondLargest = inputArray[i];
             }
         }
         return secondLargest;
--- src/removeDuplicates/RemoveDuplicate.java
+++ src/removeDuplicates/RemoveDuplicate.java
@@ -2,13 +2,13 @@ package removeDuplicates;
 
 public class RemoveDuplicate {
     public static void main(String[] args) {
-        removeDuplicates(new int []{1,1,1,2,2,3});
+        removeDuplicates(new int[]{1, 1, 1, 2, 2, 3});
         new Exception();
-        String leo="leovelbert";
+        String leo = "leovelbert";
     }
 
     public static int removeDuplicates(int[] nums) {
 
-return 0;
+        return 0;
     }
 }
